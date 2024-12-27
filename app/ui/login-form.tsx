9b72be36aec3c0cc78eee24e6355ca9210186f4e
Feitosa <feitosatecnologia@hotmail.com>
2024-12-27 19:31:46 -0300

    erron no redirecionamento

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Por favor, entre para continuar.
         </h1>
         <div className="w-full">
           <div>
@@ -47,7 +47,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
-              Password
+              Senha
             </label>
             <div className="relative">
               <input
